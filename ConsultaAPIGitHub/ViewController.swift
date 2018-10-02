f4bb1956c6f4644f7414379eee04fb55fe29e435
CaioMariano <caio.araujo.mariano@hotmail.com>
2018-10-02 19:23:39 -0300

    Initial Commit

---
+++ ConsultaAPIGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConsultaAPIGitHub
+//
+//  Created by Caio Araujo Mariano on 02/10/2018.
+//  Copyright © 2018 Caio Araujo Mariano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
